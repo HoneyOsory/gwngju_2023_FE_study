0e175c1bc4e4822e625eec4328c656b75fa65964
honeyosory <sjwon02@gmail.com>
2023-03-06 17:32:09 +0900

    444

---
+++ JavaScript_기초/js/01_js.js
@@ -0,0 +1,6 @@
+function goMessage(){
+    alert('외부 함수가 호출되었습니다');
+    // id가 p1인 태그 찾기
+    //document.getElementById("p1").innerHTML="임의의 값 넣기1";
+    // document.getElementById(mode).innerHTML="임의의 값 넣기2" + mode;
+}
